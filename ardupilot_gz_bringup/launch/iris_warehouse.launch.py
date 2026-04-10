c3568ad566991b6460773be646d88eb9470107e7
snktshrma <sharma.sanket272@gmail.com>
2026-04-10 14:41:00 +0530

    iris_depth: Added iris rgbd camera example

--- ardupilot_gz_bringup/launch/iris_warehouse.launch.py
+++ ardupilot_gz_bringup/launch/iris_warehouse.launch.py
@@ -29,7 +29,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Launch an iris quadcopter in Gazebo and Rviz."""
+"""Launch an iris quadcopter with RGBD camera in Gazebo and RViz."""
 from pathlib import Path
 
 from ament_index_python.packages import get_package_share_directory
@@ -41,6 +41,7 @@ from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch.substitutions import PathJoinSubstitution
+from launch.substitutions import PythonExpression
 
 from launch_ros.actions import Node
 
@@ -60,15 +61,11 @@ def generate_launch_description():
                         pkg_project_bringup,
                         "launch",
                         "robots",
-                        "iris_lidar.launch.py",
+                        "iris_rgbd.launch.py",
                     ]
                 ),
             ]
         ),
-        launch_arguments={
-            "world_name": LaunchConfiguration("world_name"),
-            "robot_name": LaunchConfiguration("robot_name"),
-        }.items(),
         condition=IfCondition(LaunchConfiguration("spawn_robot")),
     )
 
@@ -96,10 +93,10 @@ def generate_launch_description():
     rviz = Node(
         package="rviz2",
         executable="rviz2",
-        namespace="iris",
+        namespace=LaunchConfiguration("namespace"),
         arguments=[
             "-d",
-            f'{Path(pkg_project_bringup) / "rviz" / "iris_with_lidar.rviz"}',
+            f'{Path(pkg_project_bringup) / "rviz" / "iris_with_rgbd.rviz"}',
         ],
         condition=IfCondition(LaunchConfiguration("rviz")),
         remappings=[
@@ -108,17 +105,25 @@ def generate_launch_description():
         ],
     )
 
+    # RQT
+    rqt_topic = PythonExpression([
+        "'",
+        LaunchConfiguration("namespace"),
+        "' + '/camera/depth/image'",
+    ])
+    rqt = Node(
+        package="rqt_image_view",
+        executable="rqt_image_view",
+        arguments=[rqt_topic],
+        condition=IfCondition(LaunchConfiguration("rqt")),
+    )
+
     return LaunchDescription(
         [
             DeclareLaunchArgument(
-                "world_name",
-                default_value="warehouse",
-                description="Name for the world instance.",
-            ),
-            DeclareLaunchArgument(
-                "robot_name",
-                default_value="iris",
-                description="Name for the model instance.",
+                "namespace",
+                default_value="",
+                description="Robot namespace.",
             ),
             DeclareLaunchArgument(
                 "use_gz_sim_server",
@@ -138,9 +143,13 @@ def generate_launch_description():
             DeclareLaunchArgument(
                 "rviz", default_value="true", description="Open RViz."
             ),
+            DeclareLaunchArgument(
+                "rqt", default_value="false", description="Open rqt_image_view."
+            ),
             gz_sim_server,
             gz_sim_gui,
             robot,
             rviz,
+            rqt,
         ]
     )
